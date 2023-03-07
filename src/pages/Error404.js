a42105bb10702d2f007b333ebe1047237fc90e87
earlpappi <earlpappi@gmail.com>
2023-03-07 22:40:06 +0100

    --commit: 404 page done

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Player } from '@lottiefiles/react-lottie-player';
+import classes from '../components/Error404/Error404.module.css'
+import { Link } from 'react-router-dom';
 
 function Error404() {
   return (
-    <div>Error404</div>
+    <div className={ classes.errorMainCon }>
+      <div className={ classes.lotCon }>
+        <Player src='https://assets3.lottiefiles.com/packages/lf20_u1xuufn3.json' loop autoplay/>
+      </div>
+
+      <div>
+        <Link to='/'>Go Home</Link>
+      </div>
+    </div>
   )
 }
 
